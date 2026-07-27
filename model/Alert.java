ed46d06aa5e809f82afdcc739db06f74af53e089
sapuni2002madushika <madushikasapuni152@gmail.com>
2026-07-28 00:30:29 +0530

    Add Alert class for message handling

---
+++ model/Alert.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Alert {
+
+    private String message;
+
+    public Alert(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public String toString() {
+        return message;
+    }
+}
